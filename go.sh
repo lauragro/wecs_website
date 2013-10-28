383fe36007396450bbcc4f09fbac2eb28d296035
Paul Demchuk <pademchuk@gmail.com>
2013-10-28 13:21:11 -0700

    fixed the go script

--- go.sh
+++ go.sh
@@ -1,3 +1,5 @@
+npm install express
+
 jade --watch uncompiled/jade -o public &
 sass --watch uncompiled/sass:public/stylesheets &
 coffee -o public/javascripts -bcw uncompiled/coffeescripts/*.coffee &
