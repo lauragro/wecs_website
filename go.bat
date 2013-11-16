ba6eaeaefc2aa9978768ace7f03e5f48aeeb7133
Eric Dand <eric.c.dand@gmail.com>
2013-11-16 15:59:55 -0800

    Fixed go.bat's sass to compile to the right place
    Also began adding Bourbon mixin.

--- go.bat
+++ go.bat
@@ -4,13 +4,17 @@
 SET /P WATCH_FOR_CHANGES=Watch for changes? (y/n)
 
 IF %WATCH_FOR_CHANGES%==y (
-	START jade --watch uncompiled\jade -o public
-	START sass --watch uncompiled\sass:public\stylesheets
-	START coffee -o public\javascripts -bcw uncompiled\coffeescripts
-) ELSE IF %WATCH_FOR_CHANGES%==n (
-	jade uncompiled\jade -o public
-	sass -f uncompiled\sass\* public\stylesheets
-	coffee -o public\javascripts -bc uncompiled\coffeescripts
-) ELSE GOTO START
+	START jade --watch uncompiled/jade -o public
+	START sass --watch uncompiled/sass:public/stylesheets
+	START coffee -o public/javascripts -bcw uncompiled/coffeescripts
 
-coffee server/server.coffee
+	coffee server/server.coffee
+) ELSE IF %WATCH_FOR_CHANGES%==n (
+	jade uncompiled/jade -o public
+	sass --update uncompiled/sass:public/stylesheets
+	coffee -o public/javascripts -bc uncompiled/coffeescripts
+
+	coffee server/server.coffee
+) ELSE (
+	GOTO START
+)
